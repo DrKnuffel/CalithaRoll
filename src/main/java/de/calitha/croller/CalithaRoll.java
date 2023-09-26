554fa7a2127bac1f6049ea51e3a0af2db3f7be48
Laurent Hanzen <127599874+DrKnuffel@users.noreply.github.com>
2023-09-26 17:37:59 +0200

    Initial commit

--- src/main/java/de/calitha/croller/CalithaRoll.java
+++ src/main/java/de/calitha/croller/CalithaRoll.java
@@ -7,11 +7,14 @@ public final class CalithaRoll extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("CalithaRoll wurde aktiviert.");
+        this.getCommand("roll").setExecutor(new RollCommand(false));
+        this.getCommand("gmroll").setExecutor(new RollCommand(true));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("CalithaRoll wurde deaktiviert.");
     }
 }
---
+++ src/main/java/de/calitha/croller/RollCommand.java
@@ -0,0 +1,4 @@
+package de.calitha.croller;
+
+public class RollCommand {
+}
